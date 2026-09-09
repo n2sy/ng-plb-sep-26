a93d944055d7ce6f5658a89022d56f5c0857dc88
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2026-09-09 14:46:59 +0100

    passage de candidats de CV vers Item

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Candidat } from '../models/candidat.model';
 
 @Component({
   selector: 'app-item',
   standalone: true,
   imports: [],
   templateUrl: './item.component.html',
-  styleUrl: './item.component.css'
+  styleUrl: './item.component.css',
 })
 export class ItemComponent {
-
+  @Input() unCandidat: Candidat;
 }
--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ItemComponent } from '../item/item.component';
+import { Candidat } from '../models/candidat.model';
 
 @Component({
   selector: 'app-liste',
@@ -8,4 +9,6 @@ import { ItemComponent } from '../item/item.component';
   templateUrl: './liste.component.html',
   styleUrl: './liste.component.css',
 })
-export class ListeComponent {}
+export class ListeComponent {
+  @Input() tousCandidats: Candidat[] = [];
+}
