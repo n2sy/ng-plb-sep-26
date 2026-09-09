bba5eda5ada861ea6d01e35c00392165e89a1f9b
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2026-09-09 15:53:34 +0100

    Accounts exercice : phase 1

---
+++ src/app/accounts/home-accounts/home-accounts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-accounts',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-accounts.component.html',
+  styleUrl: './home-accounts.component.css'
+})
+export class HomeAccountsComponent {
+
+}
---
+++ src/app/accounts/item-account/item-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-account.component.html',
+  styleUrl: './item-account.component.css'
+})
+export class ItemAccountComponent {
+
+}
