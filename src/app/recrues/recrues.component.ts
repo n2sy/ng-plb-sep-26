2a61fd7cb07c2b3eb5f87f93c3a68da81f954380
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2026-09-10 12:04:51 +0100

    exercice recrues : phase 1

---
+++ src/app/recrues/recrues.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recrues',
+  standalone: true,
+  imports: [],
+  templateUrl: './recrues.component.html',
+  styleUrl: './recrues.component.css'
+})
+export class RecruesComponent {
+
+}
